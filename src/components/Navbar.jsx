01167223942fb8d896643d16e92953851bd63560
Waqar <Waqaar.qureshi1991@gmail.com>
2024-07-29 00:41:25 +0530

    Intial app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { bagImg, searchImg } from '../utils/Index'
+
+const Navbar = () => {
+  return (
+<header>
+  <nav>
+    <img src={searchImg}  alt="" className="w-12 h-12" />
+    <img src={bagImg} alt="" className="" />
+    <img src={} alt="" className="" />
+  </nav>
+</header>
+    )
+}
+
+export default Navbar
